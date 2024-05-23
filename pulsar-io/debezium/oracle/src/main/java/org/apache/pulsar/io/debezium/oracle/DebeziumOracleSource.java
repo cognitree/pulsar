7f253a7b24d3ae0eb9d48f12683ea09b27615f80
nikhilerigila09 <nikhil@cognitree.com>
2024-05-23 10:05:06 +0530

    Move debezium-core to debezium-oracle

--- pulsar-io/debezium/oracle/src/main/java/org/apache/pulsar/io/debezium/oracle/DebeziumOracleSource.java
+++ pulsar-io/debezium/oracle/src/main/java/org/apache/pulsar/io/debezium/oracle/DebeziumOracleSource.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -18,9 +18,8 @@
  */
 package org.apache.pulsar.io.debezium.oracle;
 
-import java.util.Map;
 import org.apache.kafka.connect.runtime.TaskConfig;
-import org.apache.pulsar.io.debezium.DebeziumSource;
+import java.util.Map;
 
 
 /**
